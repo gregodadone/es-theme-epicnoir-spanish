f07a54d5c48a39ad03ed255d6850753480c2f67f
Grego Dadone <grego.dadone@gmail.com>
2023-04-03 22:55:31 -0300

    En procesos nuevos sistemas batocera

--- create_missing.sh
+++ create_missing.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
 
-system_list = $(ls | cut -d : -f 1)
-system_description = ' ' read -r -a array <<< $(ls | cut -d : -f 2)
-
-cont = 0
+system_list=$(cat missing_systems.txt | cut -d : -f 1)
+readarray -t system_description <<< $(cat missing_systems.txt | awk -F': ' '{print$2}')
+readarray -t short_description <<< $(cat missing_systems.txt | awk -F': ' '{print$3}')
+readarray -t long_description <<< $(cat missing_systems.txt | awk -F': ' '{print$4}')
+cont=0
 for system in $system_list; do
-	mkdir $system
-	touch $system/theme.xml
-	cat << EOF > $system/theme.xml
-	<theme>
-	  <formatVersion>7</formatVersion>
-	  <include>./../theme.xml</include>
+    mkdir "$system"
+    touch "$system"/theme.xml
+    cat << EOF > "$system"/theme.xml
+<theme>
+  <formatVersion>7</formatVersion>
+  <include>./../theme.xml</include>
 
-	  <view name="basic, detailed, video, grid">
-	    <text name="nameLabel" extra="true">
-	      <text>${system_description[$cont]}</text>
-	    </text>
-	  </view>
+  <view name="basic, detailed, video, grid">
+    <text name="nameLabel" extra="true">
+      <text>${system_description[cont]}</text>
+    </text>
+  </view>
 
-	  <view name="system">
-	    <text name="sys_line2" extra="true">
-	      <text>${system_description[$cont]}</text>
-      	  <pos>0.12 0.130</pos>
-	    </text>
-	    <text name="shortdescription" extra="true">
-	      <text>AÑO · Descripcion</text>
-	    </text>
-	    <text name="longdescription" extra="true">
-	      <text>Descripcion</text>
-	    </text>
-	  </view>
-	</theme>
+  <view name="system">
+    <text name="sys_line2" extra="true">
+      <text>${system_description[cont]}</text>
+      <pos>0.12 0.130</pos>
+    </text>
+    <text name="shortdescription" extra="true">
+      <text>${short_description[cont]}</text>
+    </text>
+    <text name="longdescription" extra="true">
+      <text>${long_description[cont]}</text>
+    </text>
+  </view>
+</theme>
 EOF
-	cont=$((cont+1))
+    cont=$((cont+1))
 done
